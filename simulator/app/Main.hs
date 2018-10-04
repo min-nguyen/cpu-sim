417cfa0e9ce2b473f63c8d2e280c131f44976a00
min-nguyen <minhnguyen1995@googlemail.com>
2018-10-04 09:12:43 +0100

    added cpu & mem, updated instructions

--- simulator/app/Main.hs
+++ simulator/app/Main.hs
@@ -2,23 +2,57 @@ module Main where
 
 import Lib
 import Data.Word
+import qualified Data.Vector as V
 
-type Offset         = Word32
-type Address        = Word32
+type Offset             = Word32
+type Address            = Word32
 
-data Register       = Register Word32
+type Instruction        = Assembly Register Register Word32
 
-data RegisterNum    = R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8
+data Memory             = Memory (V.Vector Word32)
 
-data Instruction    = ADD  Register Register 
-                    | ADDI Register Word32
-                    | CMP  Register Register
-                    | LW   Register Offset
-                    | LI   Register Word32 
-                    | B    Address 
-                    | J    Address
-                    | BLTH Register Register Address
+data Register           = Register Word32
 
+data Registers          = Registers {
+                            r1 :: Register,
+                            r2 :: Register,
+                            r3 :: Register, 
+                            r4 :: Register, 
+                            r5 :: Register,
+                            r6 :: Register,
+                            r7 :: Register,
+                            r8 :: Register
+                        }
+
+data Assembly dest source immediate
+                        = ADD  dest source source 
+                        | ADDI dest source immediate
+                        | CMP  dest source 
+                        | LW   dest source immediate
+                        | LI   dest immediate 
+                        | B    immediate 
+                        | J    immediate
+                        | BLTH dest source immediate
+
+data InstructionResult  = InstructionResult { 
+                            output :: Word32
+                        }
+
+data CPU    = CPU {
+                memory          :: Memory,
+                registers       :: Registers,
+                pc              :: Int,
+                executionUnit   :: ExecutionUnit
+            }
+
+data ExecutionUnit = ExecutionUnit { 
+                    instruction :: Instruction
+                }
+
+
+
+
+-- executeInstruction :: Instruction -> InstructionResult
 
 main :: IO ()
 main = someFunc

d668ef6aed61df7dbf097c4f29a1c488b8af3777
min-nguyen <minhnguyen1995@googlemail.com>
2018-10-07 03:57:25 +0100

    beginning decode instructions

--- simulator/app/Main.hs
+++ simulator/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Lib
 import Data.Word
+import Data.Bits
 import qualified Data.Vector as V
 
 type Offset             = Word32
@@ -9,9 +10,11 @@ type Address            = Word32
 
 type Instruction        = Assembly Register Register Word32
 
-data Memory             = Memory (V.Vector Word32)
+type Memory             =  (V.Vector Word32)
 
-data Register           = Register Word32
+data Register           = Register { 
+                            regVal  :: Word32
+                        }
 
 data Registers          = Registers {
                             r1 :: Register,
@@ -25,9 +28,9 @@ data Registers          = Registers {
                         }
 
 data Assembly dest source immediate
-                        = ADD  dest source source 
+                        = ADD  dest source source
                         | ADDI dest source immediate
-                        | CMP  dest source 
+                        | BEQ  dest source source 
                         | LW   dest source immediate
                         | LI   dest immediate 
                         | B    immediate 
@@ -35,24 +38,52 @@ data Assembly dest source immediate
                         | BLTH dest source immediate
 
 data InstructionResult  = InstructionResult { 
-                            output :: Word32
+                            output :: (Instruction, Word32)
                         }
 
-data CPU    = CPU {
-                memory          :: Memory,
-                registers       :: Registers,
-                pc              :: Int,
-                executionUnit   :: ExecutionUnit
-            }
+data CPU                = CPU {
+                            memory          :: Memory,
+                            registers       :: Registers,
+                            pc              :: Int,
+                            executionUnit   :: ExecutionUnit
+                        }
 
-data ExecutionUnit = ExecutionUnit { 
-                    instruction :: Instruction
-                }
+data ExecutionUnit      = ExecutionUnit { 
+                            instruction :: Instruction
+                        }
+
+updateCPU :: CPU -> CPU 
+updateCPU cpu = cpu         
 
 
+fetchInstruction :: CPU -> (CPU, Word32)
+fetchInstruction cpu = (cpu, (memory cpu V.! pc cpu))
+
+decodeInstruction :: Word32 -> Word32
+decodeInstruction encodedInstruct 
+    = let opcode = shiftR encodedInstruct 24 
+      in  case opcode of    0 -> 0 -- ADD
+                            8 -> 8 -- ADDI
 
 
--- executeInstruction :: Instruction -> InstructionResult
+execInstruction :: CPU -> Instruction -> InstructionResult
+execInstruction cpu (ADD dest source_a source_b)     
+    = InstructionResult (ADD dest source_a source_b, sum . map regVal $ [source_a, source_b])
+execInstruction cpu (ADDI dest source i)  
+    = InstructionResult (ADDI dest source i, i + regVal source)
+execInstruction cpu (BEQ dest source_a source_b)     
+    = let a = regVal source_a  
+          b = regVal source_b 
+      in case () of 
+                _ | a == b        -> InstructionResult (BEQ dest source_a source_b, 1)
+                _                 -> InstructionResult (BEQ dest source_a source_b, 0)
+
+execInstruction cpu (LW dest source i)   
+    = InstructionResult (LW dest source i, (memory cpu) V.! (fromIntegral $ regVal source))
+execInstruction cpu (LI dest i)   
+    = InstructionResult (LI dest i, (memory cpu) V.! (fromIntegral $ i))
+
+
 
 main :: IO ()
 main = someFunc
